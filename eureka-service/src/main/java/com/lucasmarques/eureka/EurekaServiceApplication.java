dac4d158e7b5fc8404e9e200ddc080ca87a0a7b8
Lucas Marques <cmarques.lucas@gmail.com>
2017-05-07 16:33:05 -0300

    complete tutorial

---
+++ eureka-service/src/main/java/com/lucasmarques/eureka/EurekaServiceApplication.java
@@ -0,0 +1,18 @@
+package com.lucasmarques.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by lucascmarques on 07/05/17.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+
+}
